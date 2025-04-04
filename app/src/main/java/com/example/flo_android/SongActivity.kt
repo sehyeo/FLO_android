87ca92c9df097117dfa07550e44640a8346e0216
벨라 <icomksh@gmail.com>
2025-04-04 22:57:36 +0900

    section 2: SongActivity 이미지 변환

--- app/src/main/java/com/example/flo_android/SongActivity.kt
+++ app/src/main/java/com/example/flo_android/SongActivity.kt
@@ -7,15 +7,32 @@ import com.example.flo_android.databinding.ActivitySongBinding
 
 class SongActivity : AppCompatActivity() {
 
-    //소괄호 : 클래스를 다른 클래스로 상속을 진행할 때는 소괄호를 넣어줘야 한다.
 
-    //전역 변수
     lateinit var binding : ActivitySongBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySongBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.songDownIb.setOnClickListener {
+            finish()
+        }
+        binding.songMiniplayerIv.setOnClickListener {
+            setPlayerStatus(false)
+        }
+        binding.songPauseIv.setOnClickListener {
+            setPlayerStatus(true)
+        }
+    }
 
+    fun setPlayerStatus(isPlaying : Boolean){
+        if(isPlaying){
+            binding.songMiniplayerIv.visibility = View.VISIBLE
+            binding.songPauseIv.visibility = View.GONE
+        }
+        else{
+            binding.songMiniplayerIv.visibility = View.GONE
+            binding.songPauseIv.visibility = View.VISIBLE
+        }
     }
 }
