a20d66ea0ea6824d5d964b9acea08caae76d9dff
벨라 <icomksh@gmail.com>
2025-04-05 22:37:08 +0900

    section 5: 스플래쉬 화면 구현

--- app/src/main/java/com/example/flo_android/MainActivity.kt
+++ app/src/main/java/com/example/flo_android/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_FLO)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
