b9c865a229665b3dc4ed1a2d83dd2f06e6ae0080
벨라 <icomksh@gmail.com>
2025-04-05 13:25:35 +0900

    section 3: 라일락 클릭 시 Toast 메시지

--- app/src/main/java/com/example/flo_android/SongFragment.kt
+++ app/src/main/java/com/example/flo_android/SongFragment.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.flo_android.databinding.FragmentDetailBinding
 import com.example.flo_android.databinding.FragmentSongBinding
 
 class SongFragment : Fragment() {
@@ -19,6 +19,9 @@ class SongFragment : Fragment() {
     ): View {
         binding = FragmentSongBinding.inflate(inflater,container,false)
 
+        binding.songLalacLayout.setOnClickListener {
+            Toast.makeText(activity,"LILAC",Toast.LENGTH_SHORT).show()
+        }
         return binding.root
     }
 }
