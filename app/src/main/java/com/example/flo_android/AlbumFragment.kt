0a4c04ffffe0a18f2e433bb8f887054ea16d0f3f
벨라 <icomksh@gmail.com>
2025-04-06 18:22:15 +0900

    내 취향 mix 토글 클릭 시 이미지 변경

--- app/src/main/java/com/example/flo_android/AlbumFragment.kt
+++ app/src/main/java/com/example/flo_android/AlbumFragment.kt
@@ -5,13 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.flo_android.databinding.FragmentAlbumBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
 class AlbumFragment : Fragment() {
 
     lateinit var binding: FragmentAlbumBinding
-
+    lateinit var albumViewModel: AlbumViewModel
     private val information = arrayListOf("수록곡", "상세정보", "영상")
 
     override fun onCreateView(
@@ -20,6 +21,8 @@ class AlbumFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentAlbumBinding.inflate(inflater,container,false)
+
+        albumViewModel = ViewModelProvider(requireActivity())[AlbumViewModel::class.java]
 
         binding.albumBackIv.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction()
@@ -34,6 +37,13 @@ class AlbumFragment : Fragment() {
             tab.text = information[position]
         }.attach()
 
+        albumViewModel.isMixOn.observe(viewLifecycleOwner) { isOn ->
+            if(isOn){
+                binding.albumAlbumIv.setImageResource(R.drawable.img_album_exp3)
+            } else{
+                binding.albumAlbumIv.setImageResource(R.drawable.img_album_exp2)
+            }
+        }
         return binding.root
     }
 }
