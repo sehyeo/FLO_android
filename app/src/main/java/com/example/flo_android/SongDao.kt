96c505404ac9cc29ebf202828dd3c2c9f910d7dd
벨라 <icomksh@gmail.com>
2025-05-05 16:52:05 +0900

    좋아요 누른 곡들 보관함에 추가

--- app/src/main/java/com/example/flo_android/SongDao.kt
+++ app/src/main/java/com/example/flo_android/SongDao.kt
@@ -22,6 +22,6 @@ interface SongDao {
     @Query("UPDATE SongTable SET isLike= :isLike WHERE id = :id")
     fun updateIsLikeById(isLike: Boolean,id: Int)
 
-//    @Query("SELECT * FROM SongTable WHERE isLike= :isLike")
-//    fun getLikedSongs(isLike: Boolean): List<Song>
+    @Query("SELECT * FROM SongTable WHERE isLike= :isLike")
+    fun getLikedSongs(isLike: Boolean): List<Song>
 }
