2bca948a85c65b7b865175e708e2e7b526a0efd2
벨라 <icomksh@gmail.com>
2025-05-18 22:56:14 +0900

    둘러보기fragment 작업

--- app/src/main/java/com/example/flo_android/ChartFragment.kt
+++ app/src/main/java/com/example/flo_android/ChartFragment.kt
@@ -1,39 +1,46 @@
 package com.example.flo_android
 
-import android.R
-import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.LinearLayout
-import android.widget.ListView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.flo_android.databinding.FragmentChartBinding
 
 class ChartFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val layout = LinearLayout(requireContext()).apply {
-            orientation = LinearLayout.VERTICAL
-            setPadding(20, 20, 20, 20)
-        }
+    lateinit var binding: FragmentChartBinding
 
-        val title = TextView(requireContext()).apply {
-            text = "FLO 차트  19시 기준\n최근 24시간 집계, FLO 최고 인기곡 차트!"
-            textSize = 16f
-            setTypeface(null, Typeface.BOLD)
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentChartBinding.inflate(inflater, container, false)
 
-        layout.addView(title)
+        val chartList = listOf(
+            "10cm - 너에게 닿기를",
+            "WOODZ - Drowning",
+            "제니 (JENNIE) - like JENNIE",
+            "조째즈 - 모르시나요(PROD.로코베리)",
+            "BOYNEXTDOOR - 오늘만 I LOVE YOU",
+            "G-DRAGON - TOO BAD (feat.Anderson .Paak)",
+            "G-DRAGON - HOME SWEET HOME (feat. 태양, 대성)",
+            "황가람 - 나는 반딧불",
+            "aespa - Whiplash",
+            "우디 (Woody) - 어제보다 슬픈 오늘",
+            "오반(OVAN) - Flower",
+            "IVE (아이브) - REBEL HEART",
+            "DAY6 (데이식스) - HAPPY"
+        )
 
-        val listView = ListView(requireContext())
-        val adapter = ArrayAdapter(requireContext(), R.layout.simple_list_item_1, (0..9).map { "Item $it" })
-        listView.adapter = adapter
+        val adapter = ChartRVAdapter(chartList)
+        binding.chartListview.adapter = adapter
+        binding.chartListview.layoutManager = LinearLayoutManager(requireContext())
 
-        layout.addView(listView)
 
-        return layout
+        return binding.root
     }
 }
+
