1924bab66d48bac81ce1a9a56e7d73b8fe48fdf9
벨라 <icomksh@gmail.com>
2025-05-18 15:06:36 +0900

    section 9: 앨범 좋아요 실습

--- app/src/main/java/com/example/flo_android/SongDatabase.kt
+++ app/src/main/java/com/example/flo_android/SongDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 
-@Database(entities = [Song::class, Album::class, User::class], version = 8)
+@Database(entities = [Song::class, Album::class, User::class, Like::class], version = 9)
 abstract class SongDatabase: RoomDatabase() {
     abstract fun songDao(): SongDao
     abstract fun albumDao(): AlbumDao
