b927d5eed3f0258529286bca5ee3fcd62459a952
벨라 <icomksh@gmail.com>
2025-06-02 11:05:52 +0900

    의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -109,4 +109,12 @@ dependencies {
     //Glide
     implementation("com.github.bumptech.glide:glide:4.11.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.11.0")
+
+    implementation("com.kakao.sdk:v2-all:2.18.0") // 전체 모듈 설치, 2.11.0 버전부터 지원
+    implementation("com.kakao.sdk:v2-user:2.18.0") // 카카오 로그인 API 모듈
+    implementation("com.kakao.sdk:v2-share:2.18.0") // 카카오톡 공유 API 모듈
+    implementation("com.kakao.sdk:v2-talk:2.18.0") // 카카오톡 채널, 카카오톡 소셜, 카카오톡 메시지 API 모듈
+    implementation("com.kakao.sdk:v2-friend:2.18.0") // 피커 API 모듈
+    implementation("com.kakao.sdk:v2-navi:2.18.0") // 카카오내비 API 모듈
+    implementation("com.kakao.sdk:v2-cert:2.18.0") // 카카오톡 인증 서비스 API 모듈
 }
